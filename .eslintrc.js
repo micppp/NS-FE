45d37abf6ee178d878fbdab595dd709f329e5ae5
Michael Pallister <iam@michaelpallister.dev>
2021-05-15 16:54:45 +0100

    Getting data, basic styling added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'prettier'],
   env: {
     browser: true,
     es6: true,
   },
   parser: 'babel-eslint',
   rules: {
-    'no-console': 0,
+    'no-console': 2,
     'no-lonely-if': 2,
+    'react/react-in-jsx-scope': 0,
   },
 };
